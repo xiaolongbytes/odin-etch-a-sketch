eba3e4145ca9413f4f0a3d909a1484660074ef14
April Wang <108032964+xiaolongbytes@users.noreply.github.com>
2023-06-16 17:42:44 -0400

    Completed requirements for assignment

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-etch-a-sketch
+
+Project Prompt: https://www.theodinproject.com/lessons/foundations-etch-a-sketch
---
+++ script.js
@@ -0,0 +1,55 @@
+const DIVS_X_Y = 32;
+
+function createGrid(gridDim) {
+    const container = document.querySelector(".container");
+    for (let i = 0; i < gridDim; i++) {
+        const row = document.createElement("div");
+        row.className = "row";
+        container.appendChild(row)
+        for (let j = 0; j < gridDim; j++) {
+            const column = document.createElement("div");
+            column.className = "column";
+            container.lastElementChild.appendChild(column)
+        }
+    }
+
+    const blocks = document.querySelectorAll(".column");
+    blocks.forEach((block) => {
+    block.addEventListener("mouseover", (e) => {
+        e.target.style.background = "black";
+    })
+})
+}
+
+function deleteElementByClass(className) {
+    const elements = document.getElementsByClassName(className);
+    while (elements.length > 0) {
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+createGrid(DIVS_X_Y);
+
+const sizeButton = document.getElementById("choose-size");
+sizeButton.addEventListener("click", () => {
+    const grids = window.prompt("How many squares per side for grid? Must be less than 100.");
+    if (isNaN(grids)) {
+        return alert("Input was not an integer.")
+    }
+    if (grids == null || grids == "") {
+        return alert("No choice was made.")
+    }
+    if (parseInt(grids) > 100) {
+        return alert("Input is greater than 100 and is not allowed.")
+    }
+    if (parseInt(grids)) {
+        deleteElementByClass("column")
+        deleteElementByClass("row")
+        createGrid(grids)
+        return
+    }
+})
+
+
+
+
